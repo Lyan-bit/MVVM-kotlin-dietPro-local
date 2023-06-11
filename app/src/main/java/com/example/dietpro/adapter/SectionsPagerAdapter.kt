c9f839aa642f8987d076d34c082b451d8e4afcc6
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 19:05:51 +0100

    Update SectionsPagerAdapter.kt

--- app/src/main/java/com/example/dietpro/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/dietpro/adapter/SectionsPagerAdapter.kt
@@ -9,50 +9,60 @@ import com.example.dietpro.fragments.*
 class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     companion object {
-        private val TAB_TITLES = arrayOf("+Meal", "ListMeal", "EditMeal", "DeleteMeal", "SearchMealByDatedates", "+ProfileUser", "ListUser", "FindTotalConsumedCaloriesByDate", "FindTargetCalories", "FindBMR", "CaloriesProgress", "AddUsereatsMeal", "RemoveUsereatsMeal", "ImageRecognition")
+        private val TABTITLES = arrayOf("+Meal", "ListMeal", "EditMeal", "DeleteMeal", "SearchMealByDatedates", "+ProfileUser", "ListUser", "FindTotalConsumedCaloriesByDate", "FindTargetCalories", "FindBMR", "CaloriesProgress", "AddUsereatsMeal", "RemoveUsereatsMeal", "ImageRecognition")
     }
 
     override fun getItem(position: Int): Fragment {
         // instantiate a fragment for the page.
-            return when (position) {
-                 0 -> { 
-                    CreateProfileUserFragment.newInstance(mContext) 
-                }
-                 1 -> { 
-                    ListUserFragment.newInstance(mContext) 
-                }
-                 2 -> { 
-                    CreateMealFragment.newInstance(mContext) 
-                }
-                 3 -> { 
-                    ListMealFragment.newInstance(mContext) 
-                }
-                 4 -> { 
-                    EditMealFragment.newInstance(mContext) 
-                }
-                 5 -> { 
-                    DeleteMealFragment.newInstance(mContext) 
-                }
-                 6 -> { 
-                    SearchMealByDatedatesFragment.newInstance(mContext) 
-                }
-                 7 -> { 
-                    FindBmrFragment.newInstance(mContext) 
-                }
-                 8 -> { 
-                    FindConsumedCaloriesFragment.newInstance(mContext) 
-                }
-                 9 -> { 
-                    FindProgressFragment.newInstance(mContext) 
-                }
-                 10 -> { 
-                    RecogniseFoodFragment.newInstance(mContext) 
-                }
-                else -> CreateProfileUserFragment.newInstance(mContext) 
-             }
+        return when (position) {
+            0 -> {
+                CreateMealFragment.newInstance(mContext)
+            }
+            1 -> {
+                ListMealFragment.newInstance(mContext)
+            }
+            2 -> {
+                EditMealFragment.newInstance(mContext)
+            }
+            3 -> {
+                DeleteMealFragment.newInstance(mContext)
+            }
+            4 -> {
+                SearchMealByDatedatesFragment.newInstance(mContext)
+            }
+            5 -> {
+                CreateProfileUserFragment.newInstance(mContext)
+            }
+            6 -> {
+                ListUserFragment.newInstance(mContext)
+            }
+            7 -> {
+                FindTotalConsumedCaloriesByDateFragment.newInstance(mContext)
+            }
+            8 -> {
+                FindTargetCaloriesFragment.newInstance(mContext)
+            }
+            9 -> {
+                FindBMRFragment.newInstance(mContext)
+            }
+            10 -> {
+                CaloriesProgressFragment.newInstance(mContext)
+            }
+            11 -> {
+                AddUsereatsMealFragment.newInstance(mContext)
+            }
+            12 -> {
+                RemoveUsereatsMealFragment.newInstance(mContext)
+            }
+            13 -> {
+                ImageRecognitionFragment.newInstance(mContext)
+            }
+            else -> CreateProfileUserFragment.newInstance(mContext)
+        }
+    }
 
     override fun getPageTitle(position: Int): CharSequence {
-        return TAB_TITLES[position]
+        return TABTITLES[position]
     }
 
     override fun getCount(): Int {
